9701a30d7654480f78372e3de4b6e59d10f11818
RQuicho <raymondquicho@gmail.com>
2023-10-24 18:08:17 -0700

    adding a few styles for the home page

--- src/VendingMachine.js
+++ src/VendingMachine.js
@@ -1,19 +1,23 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import './VendingMachine.css';
 
 
 const VendingMachine = () => {
   return (
     <div>
-      <p>
-        <Link to='/chips'>Chips</Link>
-      </p>
-      <p>
-        <Link to='/soda'>Soda</Link>
-      </p>
-      <p>
-        <Link to='/cookies'>Cookies</Link>
-      </p>
+      <img src='https://media.istockphoto.com/vectors/vintage-vending-machine-with-food-and-drinks-retro-style-vector-id537518740?k=6&m=537518740&s=612x612&w=0&h=F40j5CcNBV-ogWeJXJiqM5REOGLgTkd7XQweIOD678M=' />
+      <div className='snacks'>
+        <p>
+          <Link to='/chips'>Chips</Link>
+        </p>
+        <p>
+          <Link to='/soda'>Soda</Link>
+        </p>
+        <p>
+          <Link to='/cookies'>Cookies</Link>
+        </p>
+      </div>
     </div>
   )
 }
